279eb61bd96540f967d5a4f8797d0e7bd282465c
nick <iamamiracle55@gmail.com>
2015-05-18 12:29:06 +0800

    for safety
    hide the password

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #coding:utf-8
 
 DEBUG=True
-SQLALCHEMY_DATABASE_URI='mysql://root:1994225317@localhost/flask_db?charset=utf8'
+SQLALCHEMY_DATABASE_URI='mysql://user:password@localhost/flask_db?charset=utf8'
